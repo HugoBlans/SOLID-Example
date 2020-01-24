99e0769dd5fb5da38c7dd37138774c00a7d6d140
Hugo <hugo.blans@gmail.com>
2020-01-24 15:56:18 +0100

    Add export html and csv format

--- src/Forms/ExportCSV.java
+++ src/Forms/ExportCSV.java
@@ -1,9 +1,31 @@
 package Forms;
 
+import java.lang.reflect.Field;
+
 public class ExportCSV implements ExportFormat {
     @Override
     public String format(Object obj) {
-        //TODO Formatage CSV
-        return "stub export CSV";
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table>\n");
+        try {
+            for (Field field : obj.getClass().getSuperclass().getDeclaredFields()) {
+                field.setAccessible(true);//Obligatoire si le field est private
+                sb.append(field.getName());
+                sb.append(",");
+                sb.append(field.get(obj));
+                sb.append("\n");
+            }
+            for (Field field : obj.getClass().getDeclaredFields()) {
+                field.setAccessible(true);//Obligatoire si le field est private
+                sb.append(field.getName());
+                sb.append(",");
+                sb.append(field.get(obj));
+                sb.append("\n");
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 }
--- src/Forms/ExportHTML.java
+++ src/Forms/ExportHTML.java
@@ -6,8 +6,36 @@ import java.lang.reflect.Field;
 public class ExportHTML implements ExportFormat {
     @Override
     public String format(Object obj) {
-        //TODO Formatage HTML
-
-        return "stub export HTML";
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table>\n");
+        try {
+            for (Field field : obj.getClass().getSuperclass().getDeclaredFields()) {
+                field.setAccessible(true);//Obligatoire si le field est private
+                sb.append("\t<tr>\n");
+                sb.append("\t\t<td>");
+                sb.append(field.getName());
+                sb.append("</td>\n");
+                sb.append("\t\t<td>");
+                sb.append(field.get(obj));
+                sb.append("</td>\n");
+                sb.append("\t<tr>\n");
+            }
+            for (Field field : obj.getClass().getDeclaredFields()) {
+                field.setAccessible(true);//Obligatoire si le field est private
+                sb.append("\t<tr>\n");
+                sb.append("\t\t<td>");
+                sb.append(field.getName());
+                sb.append("</td>\n");
+                sb.append("\t\t<td>");
+                sb.append(field.get(obj));
+                sb.append("</td>\n");
+                sb.append("\t<tr>\n");
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        sb.append("</table>");
+        return sb.toString();
     }
 }
